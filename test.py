7d68e1df5857103ec2b7642eae7eb5bc3112a591
Harsha Kolachina <harsha.kolachina@gmail.com>
2023-11-20 01:32:09 -0600

    Updated roster link to fall 2023

---
+++ test.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+# Get events_url from links table
+conn = sqlite3.connect('database.db')
+cur = conn.cursor()
+cur.execute("UPDATE links SET roster_url='https://docs.google.com/spreadsheets/d/1SlsQzigTr3gesOFz5HEni3wEE-AZnqB71HpgfpeqsfI/edit#gid=1671136875'")
+
+# Get roster_url from links table
+cur.execute("SELECT roster_url FROM links")
+roster_url = cur.fetchone()
+print(roster_url)

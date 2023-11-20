5d07d2c6d72480bb71704667355d2f499dfb8402
Harsha Kolachina <harsha.kolachina@gmail.com>
2023-11-20 03:16:01 -0600

    added good morning & good night functionality

--- bot.py
+++ bot.py
@@ -65,16 +65,16 @@ def message(payload):
         processed_messages.add(message_id)
 
         #Check if the message contains all possible keywords for 'upcoming events' in the user request
-        if in_list(text.lower(), ['upcoming event', 'upcoming chapter event', 'events coming up', 'future event', 'future chapter event']):
+        if in_list(text, ['upcoming event', 'upcoming chapter event', 'events coming up', 'future event', 'future chapter event']):
             #Let the user know the bot is working on the request
             send_chat_message(channel=channel_id, text="Give me a few seconds to fetch the data...")
             send_chat_message(channel=channel_id, text=upcoming_events())
         elif in_list(text, ['update the events calendar', 'update events calendar', 'update calendar', 'update calendar of event', 'update event']):
             if 'Harsha' in client.users_info(user=user_id)['user']['profile']['real_name']:
                 db_logic("events_url", text)
-                send_chat_message(channel=channel_id, text="Updated the events calendar link for you :simple_smile:\nAll user queries will now use this updated link!")
+                send_chat_message(channel=channel_id, text="Updated the events calendar link for you :slightly_smiling_face:\nAll user queries will now use this updated link!")
             else:
-                send_chat_message(channel=channel_id, text="Sorry, you don't have permission to update the events calendar :white_frowning_face:")
+                send_chat_message(channel=channel_id, text="Sorry, you don't have permission to update the events calendar :slightly_frowning_face:")
         elif 'slay' in text.lower(): #and 'Hira' in client.users_info(user=user_id)['user']['profile']['real_name']:
             send_chat_message(channel=channel_id, text="AUR NAURRR SLAYYY :fire::fire::fire:")
         elif in_list(text, ['how many', 'which', 'what']) and in_list(text, ['requirements', 'credits']):
@@ -85,9 +85,9 @@ def message(payload):
         elif in_list(text, ['update the roster', 'update roster', 'update brother list', 'update brother roster']):
             if 'Harsha' in client.users_info(user=user_id)['user']['profile']['real_name']:
                 db_logic("roster_url", text)
-                send_chat_message(channel=channel_id, text="Updated the roster link for you :simple_smile:\nAll user queries will now use this updated link!")
+                send_chat_message(channel=channel_id, text="Updated the roster link for you :slightly_smiling_face:\nAll user queries will now use this updated link!")
             else:
-                send_chat_message(channel=channel_id, text="Sorry, you don't have permission to update the roster :white_frowning_face:")
+                send_chat_message(channel=channel_id, text="Sorry, you don't have permission to update the roster :slightly_frowning_face:")
         elif in_list(text, ['chapter zoom', 'zoom link', 'zoom meeting']):
             send_chat_message(channel=channel_id, text=f"Here's the link to the chapter Zoom:\n\n {chapter_zoom()}")
         elif in_list(text, ['mailtime', 'mail time']):
@@ -95,9 +95,9 @@ def message(payload):
         elif in_list(text, ['update the budget', 'update budget', 'update the chapter budget', 'update chapter budget']):
             if 'Harsha' in client.users_info(user=user_id)['user']['profile']['real_name']:
                 db_logic("budget_url", text)
-                send_chat_message(channel=channel_id, text="Updated the budget link for you :simple_smile:\nAll user queries will now use this updated link!")
+                send_chat_message(channel=channel_id, text="Updated the budget link for you :slightly_smiling_face:\nAll user queries will now use this updated link!")
             else:
-                send_chat_message(channel=channel_id, text="Sorry, you don't have permission to update the events calendar :white_frowning_face:")
+                send_chat_message(channel=channel_id, text="Sorry, you don't have permission to update the events calendar :slightly_frowning_face:")
         elif in_list(text, ['budget', 'budget spreadsheet', 'budget sheet', 'budget google sheet']):
             send_chat_message(channel=channel_id, text=f"{budget_sheet()}")
         elif in_list(text, ['what', 'where', 'when']) and in_list(text, ["today's event", "todays event", "today's events", "todays events", "today's chapter event", "today's chapter event", "todays chapter event", "today"]):
@@ -124,6 +124,12 @@ def message(payload):
         elif in_list(text, ['bye', 'goodbye', 'cya', 'see ya', 'see you', 'later', 'adios', 'farewell']):
             #Send a reply saying goodbye, with the user's name
             send_chat_message(channel=channel_id, text="Goodbye!! <@%s> :wave:" % user_id)
+        elif in_list(text, ['goodnight', 'gn', 'night', 'good night']):
+            #Send a reply saying goodnight, with the user's name
+            send_chat_message(channel=channel_id, text="Goodnight!! <@%s> :sleeping::crescent_moon:" % user_id)
+        elif in_list(text, ['good morning', 'gm', 'morning', 'goodmorning']):
+            #Send a reply saying good morning, with the user's name
+            send_chat_message(channel=channel_id, text="Good morning!! <@%s> :sunrise:" % user_id)
         elif in_list(text, ['how are you', 'how are u', 'how r u', 'how you doin', 'how u doin']):
             #Send a reply saying you're doing well, with the user's name
             send_chat_message(channel=channel_id, text="I'm doing well, thanks for asking!! <@%s> :smile:" % user_id)
@@ -131,7 +137,7 @@ def message(payload):
             #Send a reply saying hello, with the user's name
             send_chat_message(channel=channel_id, text="Hello!! <@%s> :wave: How can I help?" % user_id)
         else:
-            send_chat_message(channel=channel_id, text="Sorry, I don't understand that command :white_frowning_face:")
+            send_chat_message(channel=channel_id, text="Sorry, I don't understand that command :slightly_frowning_face:")
 
 #Define the function when the user types /help
 @app.route('/help', methods=['POST'])
@@ -144,8 +150,9 @@ def help():
 #Define the function to return service requirements for all brothers
 def needed_requirements(user_id):
     #Grab the roster url from the database
-    roster_url = roster_ws()[0]
-    worksheets = roster_ws()[1]
+    roster = roster_ws()
+    roster_url = roster[0]
+    worksheets = roster[1]
     
     indices = []
     #Return the indices of all worksheets that contain the keywords
@@ -162,7 +169,7 @@ def needed_requirements(user_id):
         headers = roster_df(roster_url, index, worksheets, user_id)[1]
 
         if req_list.empty:
-            return "Sorry, I couldn't find your name in the roster :white_frowning_face:"
+            return "Sorry, I couldn't find your name in the roster :slightly_frowning_face:"
         
         #Get requirement as listed in the header
         try:
@@ -182,7 +189,7 @@ def needed_requirements(user_id):
                     response += f"- {col}\n"
             response += "\n\n"
     response += "*Disclaimer:* \n- The requirements are assuming you are an active brother. If you are PT LOA, please reach out to the VPO to confirm your requirements.\n"
-    response += "- If you are missing any requirements that you have already fulfilled, please reach out to the VPO. There may be discrepancies because I pull data from the roster, which may not be up-to-date yet :simple_smile:"
+    response += "- If you are missing any requirements that you have already fulfilled, please reach out to the VPO. There may be discrepancies because I pull data from the roster, which may not be up-to-date yet :slightly_smiling_face:"
     return response
 
 #Define the function to return the upcoming events
@@ -253,8 +260,9 @@ def chapter_zoom():
         
 def ritual_attendance(user_id):
     #Grab the roster url from the database
-    roster_url = roster_ws()[0]
-    worksheets = roster_ws()[1]
+    roster = roster_ws()
+    roster_url = roster[0]
+    worksheets = roster[1]
 
     response = ""
     attended = ""
@@ -268,12 +276,12 @@ def ritual_attendance(user_id):
             break
     
     if index == None:
-        return "Sorry, I couldn't find the ritual attendance sheet :white_frowning_face:"
+        return "Sorry, I couldn't find the ritual attendance sheet :slightly_frowning_face:"
     
     req_list = roster_df(roster_url, index, worksheets, user_id)[0]
 
     if req_list.empty:
-        return f"Sorry, I couldn't find your name in the roster :white_frowning_face:"
+        return f"Sorry, I couldn't find your name in the roster :slightly_frowning_face:"
     
     response += f"Here's how many ritual absences you have this semester: *{req_list.iloc[0, 1]}*\n\n"
     attended += "You have *attended* the following events:\n"
@@ -287,14 +295,15 @@ def ritual_attendance(user_id):
     
     response += f"\n{attended}\n{missed}\n\n"
     response += "*Disclaimer:* \n- If you are PT LOA, please reach out to the VPO to confirm your ritual attendance requirements.\n"
-    response += "- If a ritual you have attended is counted as an absence, please reach out to the VPO. There may be discrepancies because I pull data from the roster's ritual attendance sheet, which may not be up-to-date yet :simple_smile:"
+    response += "- If a ritual you have attended is counted as an absence, please reach out to the VPO. There may be discrepancies because I pull data from the roster's ritual attendance sheet, which may not be up-to-date yet :slightly_smiling_face:"
 
     return response
 
 def chapter_attendance(user_id):
     #Grab the roster url from the database
-    roster_url = roster_ws()[0]
-    worksheets = roster_ws()[1]
+    roster = roster_ws()
+    roster_url = roster[0]
+    worksheets = roster[1]
     
     index = None
     #Return the indices of all worksheets that contain the keywords
@@ -304,12 +313,12 @@ def chapter_attendance(user_id):
             break
 
     if index == None:
-        return "Sorry, I couldn't find the ritual attendance sheet :white_frowning_face:"
+        return "Sorry, I couldn't find the ritual attendance sheet :slightly_frowning_face:"
     
     req_list = roster_df(roster_url, index, worksheets, user_id)[0]
 
     if req_list.empty:
-        return f"Sorry, I couldn't find your name in the roster :white_frowning_face:"
+        return f"Sorry, I couldn't find your name in the roster :slightly_frowning_face:"
     
     response = f"Here's how many absences you have this semester for required chapter meetings & events: *{req_list.iloc[0, 1]}*\n\n"
     missed = "You have *missed* the following required meetings & events:\n"
@@ -326,15 +335,16 @@ def chapter_attendance(user_id):
     
     response += f"{missed}\n\n"
     response += "*Disclaimer:* \n- If you are PT LOA, please reach out to the VPO to confirm your chapter attendance requirements.\n"
-    response += "- If a chapter meeting/event you have attended is counted as an absence, please reach out to the VPO. There may be discrepancies because I pull data from the roster's chapter attendance sheet, which may not be up-to-date yet :simple_smile:"
+    response += "- If a chapter meeting/event you have attended is counted as an absence, please reach out to the VPO. There may be discrepancies because I pull data from the roster's chapter attendance sheet, which may not be up-to-date yet :slightly_smiling_face:"
 
     return response
 
 #Automatically send a user "Happy Birthday" based on the roster when the date hits
 def birthday():
     #Grab the roster url from the database
-    roster_url = roster_ws()[0]
-    worksheets = roster_ws()[1]
+    roster = roster_ws()
+    roster_url = roster[0]
+    worksheets = roster[1]
 
     index = None
     #Return the index of the worksheet that contains all active brothers
@@ -428,7 +438,7 @@ def todays_event():
 
     #If the event is empty, return a string saying so
     if events_list.empty:
-        return f"There are no events today :white_frowning_face:"
+        return f"There are no events today :slightly_frowning_face:"
     elif len(events_list) > 1:
         response += f"There are *{len(events_list)}* events today, according to the events calendar:\n\n"
     else:
